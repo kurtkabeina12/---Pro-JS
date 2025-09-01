44a2d5bbe5a3e0b4f594b9ebd88e252432470b3f
YanaAvetisyan <yanagithuby@gmail.com>
2025-09-01 16:32:28 +0300

    2

--- 1-map/1-map.js
+++ 1-map/1-map.js
@@ -9,12 +9,24 @@ const data = [{
     {
         id: 1,
         name: 'Вася'
-    },
-]
+    }
+];
 
 function deleteDuplicate(arr) {
-    const newArr = [...new Map(arr.map((item) => [item.id, item])).values()]
-    return newArr
+    const uniqueIds = new Set();
+
+    return arr.map(item => {
+        const isDuplicate = arr.find(existingItem =>
+            existingItem.id === item.id &&
+            existingItem.name === item.name
+        ) !== item;
+
+        if (!isDuplicate && !uniqueIds.has(item.id)) {
+            uniqueIds.add(item.id);
+            return item;
+        }
+        return null;
+    }).filter(item => item !== null);
 }
 
-console.log(deleteDuplicate(data))
+console.log(deleteDuplicate(data));
