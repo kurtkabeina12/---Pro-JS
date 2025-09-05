03c6b22f148d69874c70003a56f04e9ec46a4323
YanaAvetisyan <yanagithuby@gmail.com>
2025-09-05 16:30:35 +0300

    upd

--- 5-oop/5-oop.js
+++ 5-oop/5-oop.js
@@ -3,23 +3,40 @@
 const Person = function (race, name, language) {
     this.race = race,
         this.name = name,
-        this.language = language,
-        this.sayHi = function () {
-            return (this.language + this.name);
-        }
+        this.language = language
 }
 
-const Ork = new Person('orc', 'Lilu', 'ru');
-
-Ork.prototype.Bit = function () {
-    return (Ork.name + 'bit you');
+Person.prototype.sayHi = function () {
+    return (this.language + this.name);
 }
 
-
-const Elf = new Person('elf', 'Lulu', 'ru');
-
-Elf.prototype.chooseFirework = function () {
-
+const Orc = function (name, language, weapon) {
+    Person.call(this, 'orc', name, language);
+    this.weapon = weapon;
 }
-console.log(Ork.sayHi());
-console.log(Elf.sayHi());
+
+Orc.prototype = Object.create(Person.prototype);
+Orc.prototype.constructor = Orc;
+
+Orc.prototype.attack = function () {
+    console.log(this.name + ' атакует с ' + this.weapon);
+};
+
+const Elf = function (name, language, typemagic) {
+    Person.call(this, 'elf', name, language);
+    this.typemagic = typemagic;
+}
+
+Elf.prototype = Object.create(Person.prototype);
+Elf.prototype.constructor = Elf;
+
+Elf.prototype.createMagic = function () {
+    console.log(this.name + ' создает заклинание ' + this.typemagic);
+};
+
+const Orc1 = new Orc('Lilu', 'ru', 'sword');
+const Elf1 = new Elf('Lulu', 'ru', 'fire');
+console.log(Orc1)
+console.log(Elf1)
+Orc1.attack();
+Elf1.createMagic();
