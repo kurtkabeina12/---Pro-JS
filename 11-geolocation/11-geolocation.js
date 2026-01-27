4aad2539d68bec15b619c81a047459f29f920154
YanaAvetisyan <yanagithuby@gmail.com>
2026-01-28 00:06:25 +0300

    yods

--- 11-geolocation/11-geolocation.js
+++ 11-geolocation/11-geolocation.js
@@ -1,7 +1,17 @@
 'use strict'
 
 function getCurrentPosition() {
-    return new Promise(async () => {
+    return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
 }
+
+getCurrentPosition()
+    .then(position => {
+        console.log('Широта:', position.coords.latitude);
+        console.log('Долгота:', position.coords.longitude);
+        console.log('Точность:', position.coords.accuracy, 'метров');
+    })
+    .catch(error => {
+        console.error('Ошибка получения геолокации:', error.message);
+    })
